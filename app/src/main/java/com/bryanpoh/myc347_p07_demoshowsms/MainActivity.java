d1e79e49ca8562990c31d671a0118bfb8ecb55b5
Bryan Poh <zandrex00@gmail.com>
2020-06-05 12:01:46 +0800

    Initial commit

---
+++ app/src/main/java/com/bryanpoh/myc347_p07_demoshowsms/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bryanpoh.myc347_p07_demoshowsms;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
